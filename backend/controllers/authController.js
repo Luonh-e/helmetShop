4eb8a9fd4255e37626a2f5e7b4a9b7d38f869a5a
Luonh <hoangluong141202@gmail.com>
2024-10-24 22:06:36 +0700

    update: Time expire reset pw and msg of email

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,74 +1,3 @@
-// controllers/authController.js
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const crypto = require("crypto");
-const User = require("../models/User");
-const sendEmail = require("../utils/sendEmail");
-
-exports.register = async (req, res) => {
-  const { username, email, password } = req.body;
-
-  try {
-    let user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ msg: "User already exists" });
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    user = new User({
-      username,
-      email,
-      password: hashedPassword,
-    });
-
-    await user.save();
-
-    const payload = { userId: user._id };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-
-    res.json({ token });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Server error");
-  }
-};
-
-exports.login = async (req, res) => {
-  const { email, password, rememberMe } = req.body;
-
-  try {
-    let user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ msg: "Invalid credentials" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ msg: "Invalid credentials" });
-    }
-
-    const payload = { userId: user._id };
-
-    let tokenExpiry = "1h";
-    if (rememberMe) {
-      tokenExpiry = "7d";
-    }
-
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: tokenExpiry,
-    });
-
-    res.json({ token });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Server error");
-  }
-};
-
 exports.forgotPassword = async (req, res) => {
   const { email } = req.body;
 
@@ -85,13 +14,15 @@ exports.forgotPassword = async (req, res) => {
       .update(resetToken)
       .digest("hex");
 
-    user.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+    user.resetPasswordExpire = Date.now() + 7 * 60 * 60 * 1000;
 
     await user.save();
 
     const resetUrl = `${process.env.FRONTEND_BASE_URL}/reset-password/${resetToken}`;
 
-    const message = `Please click the following link to reset your password: \n\n ${resetUrl}`;
+    const message = `We heard that you lost your password. Sorry about that!
+
+But don't worry! You can use the following url to reset your password: \n\n ${resetUrl} \n\n If you don't use this link within 7 hours, it will expire. \n\n Thanks`;
 
     await sendEmail({
       email: user.email,
@@ -100,39 +31,6 @@ exports.forgotPassword = async (req, res) => {
     });
 
     res.status(200).json({ msg: "Reset email sent" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Server error");
-  }
-};
-
-exports.resetPassword = async (req, res) => {
-  const { password } = req.body;
-
-  try {
-    const resetPasswordToken = crypto
-      .createHash("sha256")
-      .update(req.params.resetToken)
-      .digest("hex");
-
-    let user = await User.findOne({
-      resetPasswordToken,
-      resetPasswordExpire: { $gt: Date.now() },
-    });
-
-    if (!user) {
-      return res.status(400).json({ msg: "Invalid or expired token" });
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(password, salt);
-
-    user.resetPasswordToken = undefined;
-    user.resetPasswordExpire = undefined;
-
-    await user.save();
-
-    res.json({ msg: "Password reset successfully" });
   } catch (err) {
     console.error(err);
     res.status(500).send("Server error");
